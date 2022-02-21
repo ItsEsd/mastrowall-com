cc9bef17112cfc0fd293e5513121a332192210e1
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-02-22 02:43:05 +0530

    Update main-ii.js

--- js/main-ii.js
+++ js/main-ii.js
@@ -185,6 +185,7 @@ function openNav() {
     $('.otservice').hide('fast');
     $('#falsesecback').hide('fast');
     $('.otserviceinfo').hide('fast');
+    document.title = "Art of Learning | MASTROWALL";
   });
 
   $("#expeducon").click(function() {
