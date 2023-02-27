72c4f81e272dc1484e80548f685a9685bafe30f0
S Das <d.soubhik@hotmail.com>
2023-02-27 13:42:21 +0530

    Update cache-routes-cachefirst-sw.js

--- pwa/sw/cache-routes-cachefirst-sw.js
+++ pwa/sw/cache-routes-cachefirst-sw.js
@@ -1,11 +1,5 @@
 /**
- * JFA PWA Toolkit
- * https://github.com/jfadev/jfa-pwa-toolkit
- * license that can be found in the LICENSE file.
- *
- * @author Jordi Fernandes Alves <jfadev@gmail.com>
- * @version 0.1
- */
+ /
 
 // Cache first strategy
 workbox.routing.registerRoute(
