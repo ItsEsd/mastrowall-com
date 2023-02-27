3833c62b09c9d719983d1a1ebfed00ad4a4d970b
S Das <d.soubhik@hotmail.com>
2023-02-28 00:55:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,10 +33,10 @@ window.addEventListener("online", function() {
 });
 
 window.addEventListener("offline", function() {
-  alert('You are offline now!\nConnect to the internet.');
-  document.body.style.backgroundImage="linear-gradient(0deg,black,white)";
-  document.body.innerHTML ="<div align='center'><div style='color:white;max-width:400px;margin-top:180px;border:none;box-shadow:0 2px 6px 0 grey;padding:40px;'><p style='font-size:60px'>&#9785;</p><h2 style='padding:10px;'>No Internet</h2></div>"+
-  "<br><br><h4 style='text-align:center;color:#1a2ca5;'>M A S T R O W A L L</h4></div>";
+  alert('You are offline now!\nConnect to the internet.');window.location.reload();
+  // document.body.style.backgroundImage="linear-gradient(0deg,black,white)";
+  // document.body.innerHTML ="<div align='center'><div style='color:white;max-width:400px;margin-top:180px;border:none;box-shadow:0 2px 6px 0 grey;padding:40px;'><p style='font-size:60px'>&#9785;</p><h2 style='padding:10px;'>No Internet</h2></div>"+
+  // "<br><br><h4 style='text-align:center;color:#1a2ca5;'>M A S T R O W A L L</h4></div>";
   document.getElementById('framecontainer').innerHTML ="<div align='center'><div style='max-width:400px;margin-top:180px;border:none;box-shadow:0 2px 6px 0 grey;padding:40px;'><p style='font-size:60px'>&#9785;</p><h2 style='padding:10px;'>No Internet</h2></div></div>";
 });
 
