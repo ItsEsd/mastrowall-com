f87a45bbc820ba6bfa54bde198b320d7047cc9f8
S Das <d.soubhik@hotmail.com>
2023-02-27 13:42:42 +0530

    Update cache-statics-sw.js

--- pwa/sw/cache-statics-sw.js
+++ pwa/sw/cache-statics-sw.js
@@ -1,11 +1,5 @@
 /**
- * JFA PWA Toolkit
- * https://github.com/jfadev/jfa-pwa-toolkit
- * license that can be found in the LICENSE file.
- *
- * @author Jordi Fernandes Alves <jfadev@gmail.com>
- * @version 0.1
- */
+ /
 
 // Cache JS, JSON and CSS (request if it is available and update the cache in the background)
 workbox.routing.registerRoute(
