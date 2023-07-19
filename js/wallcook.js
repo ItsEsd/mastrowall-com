d00da636d3b0049c7b946ee7129f9a578d04bafd
S Das <d.soubhik@hotmail.com>
2023-07-19 18:20:34 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -39,14 +39,3 @@
   if(chkck() !=true){
    /// Do restriction
   }
-  /*
-      $(document).ready(function(){
-        var cookies = document.cookie.split(";");
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = cookies[i];
-              var eqPos = cookie.indexOf("=");
-              var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-              document.cookie = name + "=true;"+"expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-          }
-      });
-      */
