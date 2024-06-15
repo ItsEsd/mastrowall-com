a0a9e308121e93e06fe1a7402224d6ab54bf31a6
soubhikdas <d.soubhik@hotmail.com>
2024-06-15 11:43:07 +0530

    Update newswid.js

--- news-widget/newswid.js
+++ news-widget/newswid.js
@@ -56,7 +56,7 @@ userFeedURLs.forEach((userUrl) => {
       // console.log(data);
       data.items.forEach((item) => {
         var content = document.getElementById("content");
-
+        content.innerHTML = "";
         var newItem = "";
         newItem +=
           '<div  id="rfitem"><a target="_blank" href="' +
