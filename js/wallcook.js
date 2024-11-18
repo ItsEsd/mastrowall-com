2f8218c5bcd6ca1f56ceaa8405a4290d667314ac
soubhikdas <d.soubhik@hotmail.com>
2024-11-19 00:37:47 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -1,41 +1,43 @@
-    var decodedCookie = decodeURIComponent(document.cookie); 
-    var ca = decodedCookie.split(';'); 
-    for (var i = 0; i < ca.length; i++) {
-       var c = ca[i].split('mwallced=')[1];
-       var d = ca[i].split('mwallcstu=')[1];
-    if(c=="true"){
-        actionforuser();
-        rmvrestrictfn();
-    }
-    else if(d=="true"){
-      actionforuser();
-    }
-      } 
-  function actionforuser(){
-    $('#intro').slideUp('slow');
+var decodedCookie = decodeURIComponent(document.cookie);
+var ca = decodedCookie.split(";");
+for (var i = 0; i < ca.length; i++) {
+  var c = ca[i].split("mwallced=")[1];
+  var d = ca[i].split("mwallcstu=")[1];
+  if (c == "true") {
+    actionforuser();
+    rmvrestrictfn();
+  } else if (d == "true") {
+    actionforuser();
   }
-  
-  function rmvrestrictfn(){
-    $('#chexid,#chkey,#conexedited').removeAttr('disabled');
-    $('#eduname,#extitle,#exdescp,#timedurhr,#timedurmin,#edueid,#crtidone').removeAttr('disabled');
-  }
-   
-  var url_string = window.location.href;
-  var url = new URL(url_string);
-  var q = url.searchParams.get("onlineTest");
-  if(q =="true"){
-      $('.otservice').show();
-      $('#falsesecback').show();
-  }
+}
+function actionforuser() {
+  $("#intro").slideUp("slow");
+}
 
-  var decodedCookie = decodeURIComponent(document.cookie); 
-  var ca = decodedCookie.split(';');
-  function chkck(){for (var i = 0; i < ca.length; i++) {
-     var c = ca[i].split('mwallced=')[1];
-     var d = ca[i].split('mwallcstu=')[1];
-  if(c =="true" || d =="true"){
-    return true;
-  }}}
-  if(chkck() !=true){
-   /// Do restriction
+function rmvrestrictfn() {
+  // $('#chexid,#chkey,#conexedited').removeAttr('disabled');
+  // $('#eduname,#extitle,#exdescp,#timedurhr,#timedurmin,#edueid,#crtidone').removeAttr('disabled');
+}
+
+// var url_string = window.location.href;
+// var url = new URL(url_string);
+// var q = url.searchParams.get("onlineTest");
+// if (q == "true") {
+//   $(".otservice").show();
+//   $("#falsesecback").show();
+// }
+
+var decodedCookie = decodeURIComponent(document.cookie);
+var ca = decodedCookie.split(";");
+function chkck() {
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].split("mwallced=")[1];
+    var d = ca[i].split("mwallcstu=")[1];
+    if (c == "true" || d == "true") {
+      return true;
+    }
   }
+}
+if (chkck() != true) {
+  /// Do restriction
+}
