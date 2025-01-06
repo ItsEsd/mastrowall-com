5ecd6db104542c8d44938fe06d2d70df114bcf9e
soubhikdas <d.soubhik@hotmail.com>
2025-01-06 16:08:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-
-const PWA_ROOT = '/pwa';
+const PWA_ROOT = "/pwa";
 
 // Import configs
-importScripts(PWA_ROOT + '/config.js');
+importScripts(PWA_ROOT + "/config.js");
 
 // Import Main Service Worker
-importScripts(PWA_ROOT + '/sw.js');
+importScripts(PWA_ROOT + "/sw.js");
+
+function preloadAllImages() {
+  const images = document.querySelectorAll("img");
+
+  images.forEach((image) => {
+    const img = new Image();
+    img.src = image.src;
+  });
+}
+
+preloadAllImages();
