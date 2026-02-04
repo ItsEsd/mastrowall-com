e26423fffbdc737c888ee54157c1ca4c8468c115
soubhikdas <d.soubhik@hotmail.com>
2026-02-04 14:11:18 +0530

    up up date .. update 🔼

--- js/wallcook.js
+++ js/wallcook.js
@@ -122,7 +122,7 @@ function showGistBox(url) {
       box.style.display = "none";
       document.body.style.overflowY = "auto";
       if (history.state === "gistbox-open") {
-        history.replaceState(null, "");
+        history.back();
       }
     };
 
@@ -137,7 +137,7 @@ function showGistBox(url) {
     document.body.appendChild(box);
   }
   if (history.state !== "gistbox-open") {
-    history.pushState({ page: "gistbox-open" }, "");
+    history.pushState("gistbox-open", "");
   }
 }
 
@@ -161,5 +161,6 @@ window.addEventListener("popstate", (ev) => {
   if (ev.state !== "gistbox-open") {
     document.getElementById("gistbox").style.display = "none";
     document.body.style.overflowY = "auto";
+    history.replaceState(null, "");
   }
 });
